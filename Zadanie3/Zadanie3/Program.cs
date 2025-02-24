2d2d41f216201fd758d6ae8865b1e562240d185a
Mateusz2007 <149246417+Mateusz2007@users.noreply.github.com>
2025-02-24 11:07:46 +0100

    Zadanie 3

---
+++ Zadanie3/Zadanie3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using Zadanie3;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Uzytkownik uzytkownik = new Uzytkownik();
+        string path = @"C:\Users\kozik\Desktop\Praktyki\Zadanie3\";
+
+        using (StreamWriter file = new StreamWriter(path + "users-" + DateTime.Now.ToString("dd_MM_yyyy_hh_mm_tt") + ".csv"))
+        {
+            file.WriteLine("LP  Imie    Nazwisko    Rok urodzenia");
+
+            for (int i = 0; i < 100; i++)
+            {
+                uzytkownik.Wygeneruj();
+                file.WriteLine(uzytkownik);
+            }
+        }
+
+        Console.WriteLine("Plik został zapisany.");
+    }
+
+
+}
---
+++ Zadanie3/Zadanie3/Uzytkownik.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie3
+{
+    internal class Uzytkownik
+    {
+        int LP = 0;
+        string Imie;
+        string Nazwisko;
+        int Rok_urodzenia;
+        public Uzytkownik() { }
+        public void Wygeneruj()
+        {
+            Random rand = new Random();
+            LP++;
+
+            switch (rand.Next(0, 4))
+            {
+                case 0: Imie = "Ania"; break;
+                case 1: Imie = "Kasia"; break;
+                case 2: Imie = "Basia"; break;
+                case 3: Imie = "Zosia"; break;
+            }
+
+            switch (rand.Next(0, 2))
+            {
+                case 0: Nazwisko = "Kowalska"; break;
+                case 1: Nazwisko = "Nowak"; break;
+            }
+
+            Rok_urodzenia = rand.Next(1990, 2001);
+        }
+
+        public override string ToString()
+        {
+            return LP + " " + Imie + " " + Nazwisko + " " + Rok_urodzenia;
+        }
+    }
+}
