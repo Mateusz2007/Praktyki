a14614b708e758848b9d80a5f6ec53025f12239c
Mateusz2007 <149246417+Mateusz2007@users.noreply.github.com>
2025-02-24 11:02:42 +0100

    Zadanie 1

---
+++ Zadanie1/Zadanie1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string path = @"C:\test\test_mat_koz.txt";
+        int wynik = 0;
+        if(File.Exists(path))
+        {
+            string tekst = File.ReadAllText(path);
+            for(int i = 0; i < tekst.Length; i++){
+                if (tekst[i] == 'a')
+                {
+                    wynik++;
+                }
+            }
+            Console.WriteLine("W tym pliku litera 'a' występuje " + wynik + " razy.");
+        }
+        else
+        {
+            Console.WriteLine("Plik nie istnieje");
+        }    
+    }
+}
