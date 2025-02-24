37d1b5f609ba74f54f9ff1c1c6f3cd9b535d1849
Mateusz2007 <149246417+Mateusz2007@users.noreply.github.com>
2025-02-24 11:03:25 +0100

    Zadanie 2

---
+++ Zadanie2/Zadanie2/Program.cs
@@ -0,0 +1,54 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string path = @"C:\Users\kozik\Desktop\Praktyki\Zadanie2\zadaniePraca.txt";
+        if(File.Exists(path))
+        {
+            string wynik = "";
+            string[] lines = File.ReadAllLines(path);
+            foreach(string ln in lines)
+            {
+                string[] words = ln.Split(' ');
+                for (int i = 0; i < words.Length; i++)
+                {
+                    if (words[i] == "praca")
+                    {
+                        wynik += "job";
+                    }
+                    else
+                    {
+                        wynik += words[i];
+                    }
+                    wynik += " ";
+                }
+
+            }
+            string new_path = "";
+            for(int i = 0; i < path.Length; i++)
+            {
+                if (path[i] == '.')
+                {
+                    break;
+                }
+                else
+                {
+                    new_path += path[i];
+                }
+            }
+            new_path += "_changed";
+            new_path += DateTime.Now.ToString("ddMMyyyy");
+            new_path += ".txt";
+            using (StreamWriter outputFile = new StreamWriter(new_path))
+            {
+                foreach (string line in lines)
+                    outputFile.WriteLine(line);
+            };
+        }
+        else
+        {
+            Console.WriteLine("Plik nie istnieje");
+        }
+
+    }
+}
